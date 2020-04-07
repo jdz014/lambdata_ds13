eb42fba2639b2863f6620c542900657caea24cdf
jdz014 <jorgezazu14@gmail.com>
2020-04-07 09:51:37 -0700

    Create an example function

---
+++ my_lambdata/my_mod.py
@@ -0,0 +1,12 @@
+
+def enlarge(n):
+    """
+    Param n is a number
+    Function will enlarge the number
+    """
+    return n * 100
+
+#x = 5
+x = int(input("Please choose a number (like 5):"))
+result = enlarge(x)
+print(result)
